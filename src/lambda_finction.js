807dd3d350e81d0ca831fdcf1f72563d05a02465
TNKNMKHK <nazo161018@gmail.com>
2024-07-31 11:26:34 +0900

    pythonに変更

--- src/lambda_finction.js
+++
@@ -1,9 +0,0 @@
-export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda! with github actions!'),
-    };
-    return response;
-  };
-  
