6db7201d0b9723bfb85b59411811db2cc9d84926
TNKNMKHK <nazo161018@gmail.com>
2024-07-31 17:50:11 +0900

    dynamodbに書き込み

--- lambda_functions/index.mjs
+++ lambda_functions/index.mjs
@@ -1,13 +1,36 @@
-export const handler = async (event) => {
-  // TODO implement
-  let body;
-  if (event.body) {
-    body = JSON.parse(event.body).greeter;
-  }
-  
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(body),
-  };
-  return response;
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+const dynamodbClient = new DynamoDBClient({
+    region: 'us-east-1',
+});
+
+// ここで、↑で初期化したDynamoDBClientを用いてDynamoDBDocumentClientを初期化
+const docClient = DynamoDBDocumentClient.from(dynamodbClient);
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const params: PutCommandInput = {
+        TableName: 'danger',
+        Item: {
+            number: 1, // ここが違う！
+            date: 1, // ここが違う！
+            what: 8, // ここが違う！
+        },
+    };
+    const command = new PutCommand(params);
+    try {
+        await docClient.send(command);
+        return {
+            statusCode: 200,
+            body: 'success',
+        };
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
 };
