996f7ad25fb7516e9558070c240f2102a721dc2a
TNKNMKHK <nazo161018@gmail.com>
2024-07-31 19:45:02 +0900

    構成を大幅に変更

--- lambda_functions/update.js
+++ lambda_functions/update.js
@@ -5,11 +5,11 @@ const dynamodbClient = new DynamoDBClient({
     region: 'us-east-1',
 });
 
-
 const docClient = DynamoDBDocumentClient.from(dynamodbClient);
 
 export const handler = async (event) => {
     // レスポンスデータの作成
+    // yyyymmddの型で date を管理する
     const current_time = new Date();
     let month = current_time.getMonth() + 1;
     if (month < 10) {
@@ -17,17 +17,21 @@ export const handler = async (event) => {
     } else {
       month = String(month);
     }
-
     const date = String(current_time.getFullYear()) + month + String(current_time.getDate());
-
+    
+    // 各パラメータをセット
     const params = {
         TableName: 'danger',
         Item: {
-            number: 1, 
-            date: Number(date), 
-            what: "安倍晋三ウンチ", 
+            number: event.number || 1, 
+            date: Number(date),
+            what: event.what || "安倍晋三ウンチ",
+            timestamp: current_time.getTime(), // タイムスタンプを追加
         },
+        ConditionExpression: 'attribute_not_exists(timestamp)', // number属性が存在しない場合にのみ書き込み
     };
+
+    // パラメータをdynamoDBに送信
     const command = new PutCommand(params);
     try {
         await docClient.send(command);
