274fc2c1d7a4e9e62f4a59c03fa18afa68f39fa6
TNKNMKHK <nazo161018@gmail.com>
2024-07-31 16:14:35 +0900

    修正

--- lambda_functions/index.mjs
+++ lambda_functions/index.mjs
@@ -1,22 +1,13 @@
-export const handler = function(event, context, callback) {
-  console.log('Received event:', JSON.stringify(event, null, 2));
-  var res ={
-      "statusCode": 200,
-      "headers": {
-          "Content-Type": "*/*"
-      }
-  };
-  
-  var greeter = 'World';
-  if (event.greeter && event.greeter!=="") {
-      greeter =  event.greeter;
-  } else if (event.body && event.body !== "") {
-      var body = JSON.parse(event.body);
-      if (body.greeter && body.greeter !== "") {
-          greeter = body.greeter;
-      }
+export const handler = async (event) => {
+  // TODO implement
+  let body;
+  if (event.body) {
+    body = JSON.parse(event.body).greeter;
   }
   
-  res.body = greeter;
-  callback(null, res);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
+  return response;
 };
