e4b95b5c603802f8b739165943d0c1079fe640c5
TNKNMKHK <nazo161018@gmail.com>
2024-07-31 13:26:51 +0900

    テスト

--- lambda_functions/index.js
+++ lambda_functions/index.js
@@ -1,8 +1,3 @@
 export const handler = async (event) => {
-  // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda! with github actions!'),
-  };
-  return response;
+  return JSON.stringify(event.queryStringParameters);
 };
