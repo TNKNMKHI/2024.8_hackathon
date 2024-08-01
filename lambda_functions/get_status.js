5bfa7169c94a7bbe8d80185fbbd5533669e9da27
TNKNMKHK <nazo161018@gmail.com>
2024-08-01 17:30:30 +0900

    送信された当日のデータのみを返却

--- lambda_functions/get_status.js
+++ lambda_functions/get_status.js
@@ -5,9 +5,34 @@ const client = new DynamoDBClient({});
 const docClient = DynamoDBDocumentClient.from(client);
 
 export const main = async () => {
+
+  // 本日の日付をyyyymmddで取得
+  const current_time = new Date();
+  let month = current_time.getMonth() + 1;
+  let day = current_time.getDate();
+  if (month < 10) {
+    month = "0" + String(month);
+  } else {
+    month = String(month);
+  }
+  if (day < 10) {
+      day = "0" + String(day);
+    } else {
+      day = String(day);
+    }
+
+  const date = String(current_time.getFullYear()) + month + day;
+
   const command = new QueryCommand({
     TableName: "danger",
-    
+    KeyConditionExpression: "#hn = :val1 ",
+    ExpressionAttributeNames:{
+        "#hn": "date"
+    },
+    ExpressionAttributeValues: {
+        ":val1":date
+    }
+
   });
 
   const response = await docClient.send(command);
