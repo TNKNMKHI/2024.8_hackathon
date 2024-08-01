2724c26c488a949879c791581c25b9d48544320f
TNKNMKHK <nazo161018@gmail.com>
2024-08-01 18:50:02 +0900

    リージョンを設定

--- lambda_functions/get_status.js
+++ lambda_functions/get_status.js
@@ -1,8 +1,10 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { QueryCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({});
-const docClient = DynamoDBDocumentClient.from(client);
+const dynamodbClient = new DynamoDBClient({
+  region: 'us-east-1',
+});
+const docClient = DynamoDBDocumentClient.from(dynamodbClient);
 
 export const handler = async (event) => {
 
@@ -37,7 +39,6 @@ export const handler = async (event) => {
     });
 
     // 検索をかけて返却
-  
     const response = await docClient.send(command);
     console.log(response);
     return response;
@@ -47,8 +48,8 @@ export const handler = async (event) => {
         statusCode: 500,
         body: JSON.stringify({
             message: 'something is wrong',
-        }),
+        })
     };
-}
+  }
 };
 
