ce772cdc2ecfe5e15c975fcbfee705135b400d06
TNKNMKHK <nazo161018@gmail.com>
2024-08-03 13:30:50 +0900

    tesuya

--- lambda_functions/get_status.js
+++ lambda_functions/get_status.js
@@ -7,7 +7,6 @@ const dynamodbClient = new DynamoDBClient({
 const docClient = DynamoDBDocumentClient.from(dynamodbClient);
 
 export const handler = async (event) => {
-
   // 日付をyyyymmddで取得
   const current_time = new Date();
   let month = current_time.getMonth() + 1;
@@ -18,23 +17,23 @@ export const handler = async (event) => {
     month = String(month);
   }
   if (day < 10) {
-      day = "0" + String(day);
-    } else {
-      day = String(day);
-    }
+    day = "0" + String(day);
+  } else {
+    day = String(day);
+  }
 
   const date = Number(String(current_time.getFullYear()) + month + day);
 
-  // dynamoDBにquaryを実行して本日分のデータを取得する
+  // dynamoDBにqueryを実行して本日分のデータを取得する
   try {
     const command = new QueryCommand({
       TableName: "danger",
       KeyConditionExpression: "#hn = :val1",
-      ExpressionAttributeNames:{
-          "#hn": "date"
+      ExpressionAttributeNames: {
+        "#hn": "date"
       },
       ExpressionAttributeValues: {
-          ":val1": date
+        ":val1": date
       }
     });
 
@@ -44,12 +43,12 @@ export const handler = async (event) => {
     return response;
 
   } catch (err) {
+    console.error("Error querying DynamoDB", err);
     return {
-        statusCode: 500,
-        body: JSON.stringify({
-            message: 'something is wrong',
-        })
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'something is wrong',
+      })
     };
   }
 };
-
